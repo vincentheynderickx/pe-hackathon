8b892b42f846de7e90d895d5d3717efd03516e07
Olivier Fondeur <fondeurolivier14@gmail.com>
2023-10-31 15:05:07 +0100

    c1

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 #partie python Olivier 
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import IPython
+
+def courbe(df):
+    df.plt()
+    plt.xlabel(df.loc[0,0])
+    plt.show()
+
