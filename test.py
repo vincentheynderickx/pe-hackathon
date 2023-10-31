601f22f04f89a6d747c8a09336dd89e16fee94e6
Olivier Fondeur <fondeurolivier14@gmail.com>
2023-10-31 15:14:38 +0100

    c2

--- test.py
+++ test.py
@@ -4,8 +4,16 @@ import pandas as pd
 import numpy as np
 import IPython
 
-def courbe(df):
+def courbe_depuis_DF(df):
     df.plt()
     plt.xlabel(df.loc[0,0])
     plt.show()
 
+def nuage_pt(df):
+    L=df.columns.to_list()
+    X=df[L[0]].to_list()
+    Y=df[L[1]].to_list()
+    plt.plot(X,Y)
+    plt.xlabel(L[0])
+    plt.ylabel(L[1])
+    plt.show()
